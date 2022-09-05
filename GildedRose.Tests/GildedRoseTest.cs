0cd0f9d042528ed3ad45a46cef0d42e1b92bf88b
Andres Ramon <andres.ramon@gmail.com>
2022-09-05 10:11:05 +0200

    Initial commit

---
+++ GildedRose.Tests/GildedRoseTest.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections.Generic;
+using GildedRoseKata;
+using Xunit;
+
+namespace GildedRose.Tests
+{
+    public class GildedRoseTest
+    {
+        [Fact]
+        public void TestRandomBehaviour()
+        {
+            Assert.True(true);
+        }
+    }
+}
