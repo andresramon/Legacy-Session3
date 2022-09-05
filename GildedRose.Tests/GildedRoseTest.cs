4033c00955f5b7fd5f717762a91038b03fc82bff
NHC <natalihc_92@hotmail.com>
2022-09-05 12:22:28 +0200

    Add new behaviour test

--- GildedRose.Tests/GildedRoseTest.cs
+++ GildedRose.Tests/GildedRoseTest.cs
@@ -11,5 +11,62 @@ namespace GildedRose.Tests
         {
             Assert.True(true);
         }
+
+        [Fact]
+        public void DecrementQualityOnce()
+        {
+            var item = new Item {Name = "TestOneItem", SellIn = 10, Quality = 20};
+            IList<Item> Items = new List<Item>{
+                item,
+            };
+           
+            var app = new GildedRoseKata.GildedRose(Items);
+            app.UpdateQuality();
+            int expected = 19;
+            Assert.Equal(expected,item.Quality);
+        }
+
+        [Fact]
+        public void NotDecrementQualityWhenZero()
+        {
+            var item = new Item {Name = "TestOneItem", SellIn = 10, Quality = 0};
+            IList<Item> Items = new List<Item>{
+                item,
+            };
+           
+            var app = new GildedRoseKata.GildedRose(Items);
+            app.UpdateQuality();
+            int expected = 0;
+            Assert.Equal(expected,item.Quality); 
+        }
+        
+        [Fact]
+        public void AgedBrieQualityIncreaseWithTime()
+        {
+            var item = new Item {Name = "Aged Brie", SellIn = 10, Quality = 0};
+            IList<Item> Items = new List<Item>{
+                item,
+            };
+           
+            var app = new GildedRoseKata.GildedRose(Items);
+            app.UpdateQuality();
+            int expected = 1;
+            Assert.Equal(expected,item.Quality); 
+        } 
+        
+        [Fact]
+        public void BackstageQualityIncreaseTwiceWithTimeWhenSellInIsFive()
+        {
+            var item = new Item {Name = "Backstage passes to a TAFKAL80ETC concert", SellIn = 5, Quality = 0};
+            IList<Item> Items = new List<Item>{
+                item,
+            };
+           
+            var app = new GildedRoseKata.GildedRose(Items);
+            app.UpdateQuality();
+            int expected = 3;
+            Assert.Equal(expected,item.Quality); 
+        }  
+        
     }
 }
