af5a0ab7df6943273b8daf5dd477fac9108738dc
UNISYS.falvarez Alvarez Elbal <UNISYS.falvarez@iberia.es>
2022-09-06 11:42:27 +0200

    Added tests for OtherName Quality Changes, test for Sulfuras Item no change in quality and sell in, tests for sellin change in items.

--- GildedRose.Tests/GildedRoseTest.cs
+++ GildedRose.Tests/GildedRoseTest.cs
@@ -121,6 +121,66 @@ namespace GildedRose.Tests
             app.UpdateQuality();
             int expected = expectedQuality;
             Assert.Equal(expected,item.Quality); 
-        } 
+        }
+        
+        [Theory]
+        // -1, 5, 10, 12
+        [InlineData("OtherName", 1, -1,0 )]
+        [InlineData("OtherName", 1, 5,0 )]
+        [InlineData("OtherName", 1, 10,0 )]
+        [InlineData("OtherName", 1, 12,0 )]
+        [InlineData("Backstage passes to a TAFKAL80ETC concert", 1, -1, 0)]
+        [InlineData("Backstage passes to a TAFKAL80ETC concert", 1, 5, 4)]
+        [InlineData("Backstage passes to a TAFKAL80ETC concert", 1, 10, 3)]
+        [InlineData("Backstage passes to a TAFKAL80ETC concert", 1, 12, 2)]
+        [InlineData("Aged Brie", 1, -1, 3)]
+        [InlineData("Aged Brie", 1, 5, 2)]
+        [InlineData("Aged Brie", 1, 10, 2)]
+        [InlineData("Aged Brie", 1, 12, 2)]
+        
+        [InlineData("Sulfuras, Hand of Ragnaros", 80, -1, 80)]
+        [InlineData("Sulfuras, Hand of Ragnaros", 80, 5, 80)]
+        [InlineData("Sulfuras, Hand of Ragnaros", 80, 10, 80)]
+        [InlineData("Sulfuras, Hand of Ragnaros", 80, 12, 80)]
+        public void sellInIterationQualityChanges(string ItemName, int quality, int sellIn, int expectedQuality)
+        {
+            var item = new Item {Name = ItemName, SellIn = sellIn, Quality = quality};
+            IList<Item> Items = new List<Item>{
+                item,
+            };
+           
+            var app = new GildedRoseKata.GildedRose(Items);
+            app.UpdateQuality();
+            int expected = expectedQuality;
+            Assert.Equal(expected,item.Quality);
+        }
+        
+        [Fact]
+        public void SulfurasSellInNoChanges()
+        {
+            var item = new Item {Name = "Sulfuras, Hand of Ragnaros", SellIn = 5, Quality = 80};
+            IList<Item> Items = new List<Item>{
+                item,
+            };
+           
+            var app = new GildedRoseKata.GildedRose(Items);
+            app.UpdateQuality();
+            int expected = 5;
+            Assert.Equal(expected,item.SellIn); 
+        }
+
+        [Fact]
+        public void NoSulfurasSellInChanges()
+        {
+            var item = new Item {Name = "Other", SellIn = 5, Quality = 80};
+            IList<Item> Items = new List<Item>{
+                item,
+            };
+           
+            var app = new GildedRoseKata.GildedRose(Items);
+            app.UpdateQuality();
+            int expected = 4;
+            Assert.Equal(expected,item.SellIn); 
+        }
     }
 }
